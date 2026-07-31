2fa28b350b12f51aa7c977ca6a301fd694f68ec1
Harsh Chafle <157059028+Harshchafle@users.noreply.github.com>
2026-07-31 16:16:19 +0530

    Time: 10 ms (66.76%), Space: 47.8 MB (87.86%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
@@ -1,47 +1,21 @@
 class Solution {
-    class Key {
-        char ch;
-        int f;
-
-        public Key(char _ch, int _f) {
-            this.ch = _ch;
-            this.f = _f;
-        }
-    }
     public int minimumPushes(String word) {
-        int ones = 0, twos = 0, threes = 0, fours = 0;
         int n = word.length();
-        PriorityQueue<Key> pq = new PriorityQueue<>((a,b) -> {
-            return b.f - a.f;
-        });
 
         int freq[] = new int[26];
         for(char ch : word.toCharArray()) {
             freq[ch-'a']++;
         }
 
-        for(int i = 0; i < 26; i++) {
-            if(freq[i] > 0) {
-                pq.offer(new Key((char)(i+'a'), freq[i]));
-            }
-        }
-
+        Arrays.sort(freq);
         int count = 0;
         int p = 0;
-        int mul = 1;
-        while(!pq.isEmpty()) {
-            Key k = pq.poll();
-            char c = k.ch;
-            int fq = k.f;
-
-            // System.out.println(c+" "+fq+" "+p+" "+mul+" "+count);
-            if(p >= 24) mul = 4;
-            else if(p >= 16) mul = 3;
-            else if(p >= 8) mul = 2;
-            count += (fq * mul);
+        for(int i = 25; i >= 0; i--) {
+            if(freq[i] == 0) break;
+            
+            count += freq[i] * (p/8 + 1);
             p++;
         }
-
         return count;
     }
 }
